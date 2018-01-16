1734aa597e1dd3ac21e19b83e7a5a55549c3fb47
artem2 <humantom88@gmail.com>
2018-01-16 16:37:01 +0300

    fix index

---
+++ src/definitions/custom/three/index.js
@@ -0,0 +1,32 @@
+// Type definitions for flow
+
+export * from "./three-core";
+
+// export * from "./three-canvasrenderer";
+// export * from "./three-colladaLoader";
+// export * from "./three-copyshader";
+// export * from "./three-css3drenderer";
+// export * from "./three-ctmloader";
+// export * from "./three-ddsloader";
+// export * from "./three-editorcontrols";
+// export * from "./three-effectcomposer";
+// export * from "./three-examples";
+// export * from "./three-fbxloader";G
+// export * from "./three-FirstPersonControls";
+// export * from "./three-maskpass";
+// export * from "./three-mtlloader";
+// export * from "./three-objloader";
+// export * from "./three-octree";
+// export * from "./three-orbitcontrols";
+// export * from "./three-orthographictrackballcontrols";
+// export * from "./three-outlinepass";
+// export * from "./three-projector";
+// export * from "./three-renderpass";
+// export * from "./three-shaderpass";
+// export * from "./three-smaapass";
+// export * from "./three-trackballcontrols";
+// export * from "./three-transformcontrols";
+// export * from "./three-vrcontrols";
+// export * from "./three-vreffect";
+
+export as namespace THREE;
